6a4c5358637c7323155ef1defdd5c75e737318a8
aarondud <85475953+aarondud@users.noreply.github.com>
2022-02-22 03:11:52 +1100

    Updated to call solving algorithm
    Can run solving algorithm from solve_v1.py

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ Simple clone of New York Times' Wordle game
 ___author___ = "https://github.com/aarondud"
 
 from wordle import Wordle
+from solver_v1 import Solver
 
 
 def main():
     game = Wordle()
-    playing = True
+    using_solver = False       # if using solver
+    solver = Solver()
 
-    while playing:
+    while using_solver:
+        current_guess = solver.next_guess()
+        print(current_guess)
+        result = game.check_guess(current_guess)
+        solver.words_to_remove(current_guess, result)
+
+    while not using_solver:
         game.get_player_guess()
-        game.check_guess(game.player_guess)
+        current_guess = game.player_attempts[game.no_guesses]
+        game.check_guess(game.list_to_string(current_guess))
 
 
 if __name__ == "__main__":
