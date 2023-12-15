9a20c09d7ee5b566a74cd669d02d6d7195a101e2
aarondud <aarondudley00@gmail.com>
2023-12-15 17:15:31 +0800

    ref: switch from useEffect to pure CSS for incorrect guess shake animation

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,23 +3,6 @@ import React from "react";
 import Tile from "./Tile.jsx";
 
 export default function Row({ wordLength, guess, currentGuess, isInvalid }) {
-  // useEffect for the shake animation
-  useEffect(() => {
-    if (isInvalid) {
-      const element = document.querySelector(".row.current.invalidWord");
-      element.classList.add("shake-animation");
-
-      // Remove the shake class after the animation ends
-      const onAnimationEnd = () => {
-        element.classList.remove("shake-animation");
-        element.removeEventListener("animationend", onAnimationEnd);
-      };
-
-      // Attach event listener for animation end
-      element.addEventListener("animationend", onAnimationEnd);
-    }
-  }, [isInvalid]);
-
   if (guess) {
     return (
       <div className="row past">
