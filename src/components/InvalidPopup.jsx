1ab66e4b7dea05cb5c8420fb4c43b922b35cf153
aarondud <aarondudley00@gmail.com>
2023-12-22 21:45:47 +0800

    feat: added invalid word popup

---
+++ src/components/InvalidPopup.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+export default function InvalidPopup({ visible }) {
+  const { darkTheme } = useContext(ThemeContext);
+  return (
+    <div
+      className={`invalid-popup ${visible ? "visible" : ""} ${
+        darkTheme ? "dark" : ""
+      }`}
+    >
+      Not in word list
+    </div>
+  );
+}
