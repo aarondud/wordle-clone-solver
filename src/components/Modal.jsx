f8e611e4338dc49a320da10e863fbd4701e3e78b
aarondud <aarondudley00@gmail.com>
2023-12-24 14:04:48 +0800

    ref: encapsulated Modal logic into EndOfGameModal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export default function Modal({ isCorrect, attemptNo, solution }) {
+const Modal = ({ isCorrect, attemptNo, solution }) => {
   const { darkTheme } = useContext(ThemeContext);
+
   return (
     <div className={`modal-overlay ${darkTheme ? "dark" : ""}`}>
-      {isCorrect && (
-        <div className={`modal-content ${darkTheme ? "dark" : ""}`}>
+      <EndOfGameModal
+        isCorrect={isCorrect}
+        attemptNo={attemptNo}
+        solution={solution}
+      />
+    </div>
+  );
+};
+
+const EndOfGameModal = ({ isCorrect, attemptNo, solution }) => {
+  const { darkTheme } = useContext(ThemeContext);
+
+  return (
+    <div className={`modal-content ${darkTheme ? "dark" : ""}`}>
+      {isCorrect ? (
+        <>
           <h1>You Win!</h1>
           <hr />
           <p>
@@ -14,32 +29,27 @@ export default function Modal({ isCorrect, attemptNo, solution }) {
             {attemptNo === 1 ? "guess" : "guesses"}.
           </p>
           <h2>{solution}</h2>
-          <p>Want to play again?</p>
-          <div className="modal-buttons">
-            <NewGameButton gameMode={"Wordl"} />
-            <NewGameButton gameMode={"Wordle"} />
-            <NewGameButton gameMode={"Wordlee"} />
-          </div>
-        </div>
-      )}
-      {!isCorrect && (
-        <div className={`modal-content ${darkTheme ? "dark" : ""}`}>
+        </>
+      ) : (
+        <>
           <h1>You Lose.</h1>
           <hr />
           <p>The solution was:</p>
           <h2>{solution}</h2>
-          <p>Want to play again?</p>
-          <div className="modal-buttons">
-            <NewGameButton gameMode={"Wordl"} />
-            <NewGameButton gameMode={"Wordle"} />
-            <NewGameButton gameMode={"Wordlee"} />
-          </div>
-        </div>
+        </>
       )}
+      <p>Want to play again?</p>
+      <div className="modal-buttons">
+        <NewGameButton gameMode="Wordl" />
+        <NewGameButton gameMode="Wordle" />
+        <NewGameButton gameMode="Wordlee" />
+      </div>
     </div>
   );
-}
+};
 
-function NewGameButton({ gameMode }) {
+const NewGameButton = ({ gameMode }) => {
   return <button className="new-game-button">{gameMode}</button>;
-}
+};
+
+export default Modal;
