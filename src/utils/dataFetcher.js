de31130339d3b775284e7047d4b41716a56fd249
aarondud <aarondudley00@gmail.com>
2023-12-26 14:38:19 +0800

    add: added 4-letter words for Wordl game mode

--- src/utils/dataFetcher.js
+++ src/utils/dataFetcher.js
@@ -1,11 +1,17 @@
 const wordData = {
+  Wordl: {
+    guesses:
+      "https://gist.githubusercontent.com/aarondud/5022901c38dc146effc2ef0f724f3181/raw/53b42c0e1915c3d81078a7e7b0ff6167e2c4e399/wordle-4letter-words.txt",
+    solutions:
+      "https://gist.githubusercontent.com/aarondud/5022901c38dc146effc2ef0f724f3181/raw/53b42c0e1915c3d81078a7e7b0ff6167e2c4e399/wordle-4letter-solutions.txt",
+  },
   Wordle: {
     guesses:
       "https://gist.githubusercontent.com/aarondud/77aaceafa65ec74ff82250f26d5e77ce/raw/623374aba869301b34d124d87207c34379a00dd5/wordle-5letter-words.txt",
     solutions:
       "https://gist.githubusercontent.com/aarondud/77aaceafa65ec74ff82250f26d5e77ce/raw/623374aba869301b34d124d87207c34379a00dd5/wordle-5letter-solutions.txt",
   },
-  "Wordle+": {
+  Wordlee: {
     guesses:
       "https://gist.githubusercontent.com/aarondud/e1ad0f8affe04fe708d3b22c90824324/raw/01c1cbd61be427bc1ed48192d5450d922ece0f48/wordle-6letter-words.txt",
     solutions:
@@ -17,7 +23,6 @@ const fetchData = async (gameMode) => {
   try {
     const data = {};
     const guessesText = await fetchText(wordData[gameMode].guesses);
-
     const solutionText = await fetchText(wordData[gameMode].solutions);
     const randomIndex = Math.floor(Math.random() * solutionText.length);
 
